3d5d4a828334909c453f05d14ff78358cd35b408
Alan Paccor <alan@Alans-Air.attlocal.net>
2023-09-16 17:02:04 -0400

    added webpack npm

--- server/routes/webpack.config.js
+++ server/routes/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest'); // Add this line
 
 module.exports = {
   // Your Webpack configuration settings...
@@ -7,6 +8,20 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html', // Path to your HTML template file
       filename: 'index.html', // Name of the output HTML file
+    }),
+    new WebpackPwaManifest({
+      name: 'Your App Name',
+      short_name: 'App Name',
+      description: 'Description of your app',
+      // Add more options as needed
+      // For example, icons, background color, display mode, etc.
+      icons: [
+        {
+          src: 'path/to/icon.png',
+          sizes: [96, 128, 192, 256, 384, 512], // Adjust sizes as needed
+        },
+      ],
+      // Other options...
     }),
   ],
 };
